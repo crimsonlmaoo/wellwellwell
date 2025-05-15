feadaa4c6572cb9ca14e7540d42014519bc40147
crimson <151975517+crimsonlmaoo@users.noreply.github.com>
2025-05-14 19:26:10 -0500

    Update users.js

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const usersFilePath = path.join(__dirname, 'users.json');
+const usersFilePath = path.join(__dirname, '/users.json');
 
 const loadUsers = () => {
     if (!fs.existsSync(usersFilePath)) {
